3939afa4df5feab3ac04119fcfd35c5d5f31dba8
Ralph Mallett <rmallet6@ford.com>
2023-11-03 14:49:53 +0000

    initial commit

---
+++ src/main/java/com/ralphm10/starwars/controller/FilmController.java
@@ -0,0 +1,17 @@
+package com.ralphm10.starwars.controller;
+
+import com.ralphm10.starwars.service.FilmService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilmController {
+    private FilmService filmService;
+
+    @GetMapping("/films")
+    public String getCount(@RequestParam String character) {
+        int count = filmService.getFilmCount(character);
+        return character + " appeared in " + count + " films";
+    }
+}
---
+++ src/main/java/com/ralphm10/starwars/service/FilmService.java
@@ -0,0 +1,7 @@
+package com.ralphm10.starwars.service;
+
+public class FilmService {
+    public int getFilmCount(String character) {
+        return 0;
+    }
+}
