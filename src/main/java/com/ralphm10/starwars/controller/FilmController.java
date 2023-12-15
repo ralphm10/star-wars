b00ac0ea18ca651f92268d991489a78d99c0ba10
Ralph Mallett <rmallet6@ford.com>
2023-12-15 13:44:37 +0000

    add retrofit

--- src/main/java/com/ralphm10/starwars/controller/FilmController.java
+++ src/main/java/com/ralphm10/starwars/controller/FilmController.java
@@ -1,9 +1,13 @@
 package com.ralphm10.starwars.controller;
 
+import com.ralphm10.starwars.models.entity.Person;
 import com.ralphm10.starwars.service.FilmService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class FilmController {
@@ -14,8 +18,14 @@ public class FilmController {
     }
 
     @GetMapping("/films-webclient")
-    public String getCount(@RequestParam String character) {
+    public String getCountWebClient(@RequestParam String character) {
         int count = filmService.getCountWithWebClient(character);
         return character + " appeared in " + count + " films";
+    }
+
+    @GetMapping("/films-retrofit")
+    public List<Person> getCountRetrofit() throws IOException {
+
+        return filmService.getPeopleWithRetrofit();
     }
 }
